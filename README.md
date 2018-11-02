a720c608547331a6c7d0fddcb6bfb6ff8825ba90
Steve Tingiris <steve@dabblelab.com>
2018-11-02 14:16:26 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,81 @@
+# Alexa Presentation Language (APL) Starter Template
+
+This is an Alexa skill template that can be used as a starting point for building, or learning how to build skills that uses the Alexa Presentation Language (APL). 
+
+This template uses the [Alexa Skills Kit for Node.js](https://github.com/alexa/alexa-skills-kit-sdk-for-nodejs) version 2.0 and is designed to be used with the [Alexa Skills Kit CLI](https://developer.amazon.com/docs/smapi/ask-cli-intro.html).
+
+## Introduction
+
+The Alexa Presentation Language or APL is a language that lets you enhance your Alexa skills with full-featured, responsive, and interactive screen displays. Using the APL is works similar creating Web pages and provides similar support for HTML, CSS, and JavaScript.
+
+## Steps to enable APL support in your skill
+
+1. Add the APL interface directive
+
+    Add the following code in the `mainfest` > `apis` > `custom` section of the `skill.json`.
+
+    ```json
+      "interfaces": [
+        {
+          "type": "ALEXA_PRESENTATION_APL"
+        }
+      ]
+    ```
+
+2. Add an APL template named `main.json`
+
+    ```json
+    {
+        "type": "APL",
+        "version": "1.0",
+        "import": [
+            {
+                "name": "alexa-layouts",
+                "version": "1.0.0"
+            }
+        ],
+        "mainTemplate": {
+            "parameters": [
+                "payload"
+            ],
+            "items": [
+                {
+                    "type": "Text",
+                    "text": "Hello World"
+                }
+            ]
+        }
+    }
+    ```
+
+3. Require the template in your `index.js` file
+
+    ```javascript
+    const main = require('./main.json');
+    ```
+
+4. Add the `Alexa.Presentation.APL.RenderDocument` directive to the responseBuilder
+
+    ```javascript
+    const LaunchRequestHandler = {
+      canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+      },
+      handle(handlerInput) {
+        const speechText = 'Hello there. What is your name?';
+        const repromptText = 'Can you tell me your name?';
+
+        return handlerInput.responseBuilder
+          .speak(speechText)
+          .reprompt(repromptText)
+          .withSimpleCard('Example Card Title', "Example card body content.")
+          .addDirective({
+            type: 'Alexa.Presentation.APL.RenderDocument',
+            version: '1.0',
+            document: main,
+            datasources: {}
+          })
+          .getResponse();
+      },
+    };
+    ```
---
+++ lambda/custom/index.js
@@ -0,0 +1,110 @@
+/* eslint-disable  func-names */
+/* eslint-disable  no-console */
+const main = require('./main.json');
+const Alexa = require('ask-sdk-core');
+
+const LaunchRequestHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+  },
+  handle(handlerInput) {
+    const speechText = 'Hello there. What is your name?';
+    const repromptText = 'Can you tell me your name?';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(repromptText)
+      .withSimpleCard('Example Card Title', "Example card body content.")
+      .addDirective({
+        type: 'Alexa.Presentation.APL.RenderDocument',
+        version: '1.0',
+        document: main,
+        datasources: {}
+      })
+      .getResponse();
+  },
+};
+
+const MyNameIsIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'MyNameIsIntent';
+  },
+  handle(handlerInput) {
+
+    const nameSlot = handlerInput.requestEnvelope.request.intent.slots.name.value;
+    const speechText = `Hello ${nameSlot}. It's nice to meet you.`;
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .getResponse();
+  },
+};
+
+const HelpIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
+  },
+  handle(handlerInput) {
+    const speechText = 'You can introduce yourself by telling me your name';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .getResponse();
+  },
+};
+
+const CancelAndStopIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
+        || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
+  },
+  handle(handlerInput) {
+    const speechText = 'Goodbye!';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .getResponse();
+  },
+};
+
+const SessionEndedRequestHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
+  },
+  handle(handlerInput) {
+    console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+
+    return handlerInput.responseBuilder.getResponse();
+  },
+};
+
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${error.message}`);
+
+    return handlerInput.responseBuilder
+      .speak('Sorry, I can\'t understand the command. Please say again.')
+      .reprompt('Sorry, I can\'t understand the command. Please say again.')
+      .getResponse();
+  },
+};
+
+const skillBuilder = Alexa.SkillBuilders.custom();
+
+exports.handler = skillBuilder
+  .addRequestHandlers(
+    LaunchRequestHandler,
+    MyNameIsIntentHandler,
+    HelpIntentHandler,
+    CancelAndStopIntentHandler,
+    SessionEndedRequestHandler
+  )
+  .addErrorHandlers(ErrorHandler)
+  .lambda();
